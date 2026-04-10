bb3d74c3b2facf2fc559ed44d32d072d6c663b89
Mahmoud abuistaiteh <mabuistaiteh@gmail.com>
2026-04-09 21:57:14 -0400

    backend fmt fix

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "amzn-project-three-terraform-abuis2580"
-    key            = "third=terraform-project/terraform.tfstate"
-    region         = "us-east-1"
+    bucket       = "amzn-project-three-terraform-abuis2580"
+    key          = "third=terraform-project/terraform.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
