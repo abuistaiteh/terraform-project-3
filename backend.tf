79d101128341d0f4845553a29c34f966195074fc
Mahmoud abuistaiteh <mabuistaiteh@gmail.com>
2026-04-09 21:35:14 -0400

    terraform with remote backend

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "amzn-project-three-terraform-abuis2580"
+    key = "third=terraform-project/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
