700bf8656da3f73c6286230228292f8864bcf0fc
Mahmoud abuistaiteh <mabuistaiteh@gmail.com>
2026-04-09 21:47:20 -0400

    fixed fmt

--- versions.tf
+++ versions.tf
@@ -3,9 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
-
